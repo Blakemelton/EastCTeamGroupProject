f93af91139ae33502501508a607e32da4d782154
blakemelton <blakemelton2591@gmail.com>
2021-08-15 16:09:23 -0400

    Group project

---
+++ GroupProject/Task4.java
@@ -0,0 +1,32 @@
+package GroupProject;
+
+public class Task4 {
+public static void main(String[] args) {
+	
+	// Write a program to swap 2 numbers without a temporary variable?
+
+	
+	
+	int a=10;
+	
+	int b=20;
+	
+	
+	
+	a=a+b; //30
+	
+	b=a-b; //30-20=10
+	
+	a=a-b; // 30-10=20
+	
+	
+	System.out.println("a = "+a);
+	
+	
+	System.out.println("b = "+b);
+	
+	
+}
+
+}
+
